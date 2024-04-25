c02d42fe2ca8bbd0c1d7f5fdae0dc4d5a52f6b93
Abhishek Gupta <109204269+Abhishek-Gupta-1@users.noreply.github.com>
2024-04-25 18:19:13 +0530

    Update Navbar.tsx

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
                     className="flex items-center space-x-3 rtl:space-x-reverse"
                 >
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                        DNS Weather Forecast
+                        My Weather Forecast
                     </span>
                 </a>
                 <button
